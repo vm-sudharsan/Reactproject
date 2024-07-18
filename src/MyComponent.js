3936a840c4b14988a651d37c2f145c5833884ef6
sudharsanprakalathanvm <sudharsanprakalathanvm@gmail.com>
2024-07-18 16:41:37 +0530

    Initial Commit

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React  from "react";
+
+function MyComponent()
+{
+    return(
+        <div>
+            <h1>Hello Everyone !</h1>
+            <p>How it's Going on .... </p>
+        </div>
+    );
+}
+
+export default MyComponent;
